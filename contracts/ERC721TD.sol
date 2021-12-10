3bdf4b92ea655eb276a774dca4763189cc2eff67
lilyan <lilyan.bastien@gmail.com>
2021-12-10 12:09:17 +0100

    exo6a

--- contracts/ERC721TD.sol
+++ contracts/ERC721TD.sol
@@ -8,8 +8,170 @@ import "./IExerciceSolution.sol";
 
 contract ERC721TD is ERC721 {
 
-    // set contract name and ticker. 
-    constructor (string memory name_, string memory symbol_, address to_, uint256 tokenid_) public ERC721(name_, symbol_){
-        _mint(to_, tokenid_);
-    }
+    struct Animal {
+		uint256 id;
+		string name;
+		bool wings;
+		uint legs; 
+		uint sex;
+		bool isForSale;
+		uint256 price;
+		uint256 parent1;
+		uint256 parent2;
+		bool canReproduce;
+		uint256 reproductionPrice;
+		address payable authorizedUser;
+	}
+
+	uint256 private _tokenNumber;
+	uint256 private _priceToBecomeBreeder;
+	address private _owner;
+	mapping(uint256 => Animal) public _tokens;
+	mapping(address => bool) public _breeders;
+
+	modifier onlyBreeder(){
+	    require(_breeders[msg.sender], "Only a breeder");
+	    _;
+	}
+
+	modifier onlyOwner(){
+	    require(_owner == msg.sender, "Only the contract owner");
+	    _;
+	}
+
+	modifier onlyAnimalOwner(uint256 tokenId){
+	    require(ownerOf(tokenId) == msg.sender, "Only the animal owner");
+	    _;
+	}
+
+	constructor(string memory _name, string memory _symbol) public ERC721(_name, _symbol){
+		_tokenNumber = 0;
+		_priceToBecomeBreeder = 0.1 ether;
+		_owner = msg.sender;
+		_breeders[_owner] = true;
+	}
+
+	function isBreeder(address account) external view returns (bool){
+		return _breeders[account];
+	}
+
+	function registrationPrice() external view returns (uint256){
+		return _priceToBecomeBreeder;
+	}
+
+	function registerMeAsBreeder() external	payable{
+		require(msg.value == _priceToBecomeBreeder, "Wrong amount to become breeder");
+		_breeders[msg.sender] = true;
+	}
+
+	function declareAnimal(uint sex, uint legs, bool wings, string calldata name) external onlyBreeder returns (uint256){
+		_tokenNumber++;
+		_mint(msg.sender, _tokenNumber);
+		Animal memory newAnimal = Animal(_tokenNumber, name, wings, legs, sex, false, 0, 0, 0, false, 0, address(0));
+		_tokens[_tokenNumber] = newAnimal;
+		return _tokenNumber;
+	}
+
+	function getAnimalCharacteristics(uint animalNumber) external view returns (string memory _name, bool _wings, uint _legs, uint _sex){
+		require(animalNumber <= _tokenNumber, "Id not found");
+		require(animalNumber > 0, "Id not found");
+		Animal memory animal = _tokens[animalNumber];
+		return (animal.name, animal.wings, animal.legs, animal.sex);
+	}
+
+	function declareDeadAnimal(uint animalNumber) external onlyAnimalOwner(animalNumber){
+		_burn(animalNumber);
+		delete _tokens[animalNumber];
+	}
+
+	// Selling functions
+
+	function isAnimalForSale(uint animalNumber) external view returns (bool){
+		return _tokens[animalNumber].isForSale;
+	}
+
+	function animalPrice(uint animalNumber) external view returns (uint256){
+		return _tokens[animalNumber].price;
+	}
+
+	function buyAnimal(uint animalNumber) external payable{
+		Animal memory animal = _tokens[animalNumber];
+
+		require(animal.isForSale, "Animal is not for sale");
+		require(msg.value == animal.price);
+
+		address animalOwner = ownerOf(animalNumber);
+
+		//give eth to current owner
+		(bool sent, bytes memory data) = animalOwner.call{value: msg.value}("");
+		require(sent, "Failed to transfer Ether");
+
+		//transfer token to new owner
+		_transfer(animalOwner, msg.sender, animalNumber);
+
+		//reset sale
+		_tokens[animalNumber].isForSale = false;
+		_tokens[animalNumber].price = 0;
+	}
+
+	function offerForSale(uint animalNumber, uint price) external onlyAnimalOwner(animalNumber){
+		_tokens[animalNumber].isForSale = true;
+		_tokens[animalNumber].price = price;
+	}
+
+	function lastToken() external view returns (uint256){
+		return _tokenNumber;
+	}
+
+	// Reproduction functions
+
+	function declareAnimalWithParents(uint sex, uint legs, bool wings, string calldata name, uint parent1, uint parent2) external onlyBreeder returns (uint256){
+		require(msg.sender == this.authorizedBreederToReproduce(parent2), "User not allowed to reproduce");
+		require(ownerOf(parent1) != address(0), "Parent 1 no exist");
+		require(ownerOf(parent2) != address(0), "Parent 2 no exist");
+		_tokenNumber++;
+		_mint(msg.sender, _tokenNumber);
+		Animal memory newAnimal = Animal(_tokenNumber, name, wings, legs, sex, false, 0, parent1, parent2, false, 0, address(0));
+		_tokens[_tokenNumber] = newAnimal;
+
+		_tokens[parent2].authorizedUser = address(0);
+
+		return _tokenNumber;
+	}
+
+	function getParents(uint animalNumber) external view returns (uint256, uint256){
+		Animal memory animal = _tokens[animalNumber];
+		return (animal.parent1, animal.parent2);
+	}
+
+	function canReproduce(uint animalNumber) external view returns (bool){
+		return _tokens[animalNumber].canReproduce;
+	}
+
+	function reproductionPrice(uint animalNumber) external view	returns (uint256){
+		return _tokens[animalNumber].reproductionPrice;
+	}
+
+	function offerForReproduction(uint animalNumber, uint priceOfReproduction) external onlyAnimalOwner(animalNumber) returns (uint256){
+		require(priceOfReproduction > 0);
+		_tokens[animalNumber].canReproduce = true;
+		_tokens[animalNumber].reproductionPrice = priceOfReproduction;
+		return animalNumber;
+	}
+
+	function authorizedBreederToReproduce(uint animalNumber) external view returns (address){
+		return _tokens[animalNumber].authorizedUser;
+	}
+
+	function payForReproduction(uint animalNumber) external payable{
+		Animal memory animal = _tokens[animalNumber];
+		require(animal.canReproduce, "Animal can not reproduce");
+		require(animal.reproductionPrice == msg.value , "Wrong amount");
+		require(animal.authorizedUser == address(0) , "Already in reproduction");
+
+		(bool sent, bytes memory data) = ownerOf(animalNumber).call{value: msg.value}("");
+		require(sent, "Failed to transfer Ether");
+
+		_tokens[animalNumber].authorizedUser = msg.sender;
+	}    
 }
--- migrations/1_td_deploy.js
+++ migrations/1_td_deploy.js
@@ -11,7 +11,7 @@ module.exports = (deployer, network, accounts) => {
     deployer.then(async () => {
         await deployTDToken(deployer, network, accounts); 
         await deployEvaluator(deployer, network, accounts); 
-        await setPermissionsAndRandomValues(deployer, network, accounts); 
+        // await setPermissionsAndRandomValues(deployer, network, accounts);
 		await Exercice(deployer, network, accounts);
         await deployRecap(deployer, network, accounts); 
     });
@@ -26,7 +26,7 @@ async function deployEvaluator(deployer, network, accounts) {
 	// Evaluator = await evaluator.new(TDToken.address)
 	Evaluator = await evaluator.at("0xa0b9f62A0dC5cCc21cfB71BA70070C3E1C66510E") 
 	// Evaluator2 = await evaluator2.new(TDToken.address)
-	Evaluator = await evaluator.at("0x4f82f7A130821F61931C7675A40fab723b70d1B8")
+	Evaluator2 = await evaluator.at("0x4f82f7A130821F61931C7675A40fab723b70d1B8")
 }
 
 async function setPermissionsAndRandomValues(deployer, network, accounts) {
@@ -64,13 +64,59 @@ async function deployRecap(deployer, network, accounts) {
 
 
 async function Exercice(deployer, network, accounts) {
-	MyERC721 = await ERC721TD.new("TLANI", "TLA","0xa0b9f62A0dC5cCc21cfB71BA70070C3E1C66510E", 1);
-	await Evaluator.submitExercice(MyERC721.address);
+
+	//deploy ERC721
+	MyERC721 = await ERC721TD.new("TLANI","TLA");
+	//note
 	getBalance = await TDToken.balanceOf(accounts[0]);
-	console.log("Init balance : " + getBalance.toString());
-	//exo1
-
-
+	
+	//submit
+	await Evaluator.submitExercice(MyERC721.address);
+	
+	//exercice 1
+	await MyERC721.declareAnimal(0, 4, 2, "Lilyan");
+	await MyERC721.transferFrom(accounts[0], Evaluator.address, 1);
 	await Evaluator.ex1_testERC721();
+	console.log("Balance after exo1 : " + getBalance.toString());
+	
+	//exercice 2a
+	await Evaluator.ex2a_getAnimalToCreateAttributes();
+
+	//exercice 2b
+	const sexe = await Evaluator.readSex(accounts[0]);
+	const jambes = await Evaluator.readLegs(accounts[0]);
+	const ailes = await Evaluator.readWings(accounts[0]);
+	const nom = await Evaluator.readName(accounts[0]);
+
+	await MyERC721.declareAnimal(sexe, jambes, ailes, nom);
+	await MyERC721.transferFrom(accounts[0], Evaluator.address, 2);
+	await Evaluator.ex2b_testDeclaredAnimal(2);
+	console.log("Balance after exo2 : " + getBalance.toString());
+
+	//exercice 3
+	await Evaluator.ex3_testRegisterBreeder();
+	console.log("Balance after exo3 : " + getBalance.toString());
+
+	//exercice 4
+	await Evaluator.ex4_testDeclareAnimal();
+	console.log("Balance after exo4 : " + getBalance.toString());
+
+	//exercice 5
+	await Evaluator.ex5_declareDeadAnimal();
+	console.log("Balance after exo5 : " + getBalance.toString());
+	
+	//exercice 6a
+	await Evaluator.ex6a_auctionAnimal_offer();
+
+	/*
+	//exercice 6b
+	console.log("Balance after exo6a : " + getBalance.toString());
+	await MyERC721.declareAnimal(0, 4, 2, "Lilyan");
+	const num = await MyERC721.lastToken();
+	await MyERC721.offerForSale(num, 1000)
+
+	await Evaluator.ex6b_auctionAnimal_buy(num);
+	console.log("Balance after exo6b : " + getBalance.toString());
+	*/
 	
 }
